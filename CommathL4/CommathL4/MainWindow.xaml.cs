a3e5e5258001a48a831a9c58d63fc71bb5cbefaf
artamonovalex18 <artamonovalex18@gmail.com>
2018-12-16 23:08:08 +0300

    Completed 4th lab

---
+++ CommathL4/CommathL4/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using CommathLab3;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CommathL4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Dictionary<string, Func<double, double, double>> Functions = new Dictionary<string, Func<double, double, double>>
+        {
+            { "y' = x^3 + x + 3y/x", (a,b) => a*a*a + a + 3*b/a},
+            { "y' = cos(x)", (a,b) => Math.Cos(a) },
+            { "y' = -2y", (a,b) => -2 * b }
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FunctionBox.ItemsSource = Functions.Keys;
+            FunctionBox.SelectedIndex = 0;
+            Plot.Model = new PlotModel();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (GetInput(out double x0, out double y0, out double xN, out double accuracy, out string function))
+            {
+
+
+                double n = (xN - x0 + 1) * 10;
+                double step = (xN - x0) / n;
+
+                double[] y = MilneCalculator.Calculate(Functions[function], x0, y0, step, n, accuracy, out double[] x);
+                LagrangePolynomial pol = new LagrangePolynomial(x, y);
+
+                if (Plot.Model.Series.Count != 0)
+                {
+                    Plot.Model.Series.Clear();
+                }
+
+                //Supposed option
+                Plot.Model.Series.Add(new FunctionSeries(pol.Compute, x0, xN, step));
+
+                //-------------------------------------------------------
+                //Alternative option if something went wrong
+                //-------------------------------------------------------
+                //OxyPlot.Series.LineSeries lineSeries = new OxyPlot.Series.LineSeries();
+                //for (int counter = 0; counter < y.Length; counter++)
+                //{
+                //    lineSeries.Points.Add(new DataPoint(x[counter], y[counter]));
+                //}
+                //Plot.Model.Series.Add(lineSeries);
+
+
+                Plot.Model.InvalidatePlot(true);
+            }
+        }
+
+        private bool GetInput(out double x0, out double y0, out double xN, out double accuracy, out string function)
+        {
+            x0 = 0;
+            y0 = 0;
+            xN = 0;
+            accuracy = 0;
+            function = FunctionBox.Text;
+
+            if (XInputBox.Text == "" || YInputBox.Text == "" || XNInputBox.Text == "" || AccInputBox.Text == "")
+            {
+                ErrorBox.Text = "Все поля должны быть \nзаполнены";
+                return false;
+            } else if (!double.TryParse(XInputBox.Text, out x0) || !double.TryParse(YInputBox.Text, out y0) || !double.TryParse(XNInputBox.Text, out xN) || !double.TryParse(AccInputBox.Text, out accuracy))
+            {
+                ErrorBox.Text = "Все введённые значения \nдолжны соответствовать \nтипу double";
+                return false;
+            } else
+            {
+                ErrorBox.Text = "";
+                return true;
+            }
+        }
+    }
+}
---
+++ CommathL4/CommathL4/MilneCalculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommathL4
+{
+    public static class MilneCalculator
+    {
+
+        public static double RungeKuttaMethod(Func<double,double,double> diffFunc, double x0, double y0, double step)
+        {
+            double k1 = diffFunc(x0, y0);
+            double k2 = diffFunc(x0 + step / 2, y0 + k1 * step / 2);
+            double k3 = diffFunc(x0 + step / 2, y0 + k2 * step / 2);
+            double k4 = diffFunc(x0 + step, y0 + k3 * step);
+
+            double nextY = y0 + step * (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+
+            return nextY;
+        }
+
+        public static double[] Calculate(Func<double, double, double> diffFunc, double x0, double y0, double step, double n, double accuracy, out double[] xArray)
+        {
+            List<double> y = new List<double> { y0 };
+            List<double> x = new List<double> { x0 };
+
+            for(int counter = 0; counter < 3; counter++)
+            {
+                y.Add(RungeKuttaMethod(diffFunc, x[counter], y[counter], step));
+                x.Add(x[counter] + step);
+            }
+
+            for(int counter = 3; counter < n; counter++)
+            {
+                y.Add(MilneMethod(diffFunc, x.ToArray(), y.ToArray(), step, accuracy));
+                x.Add(x[counter] + step);
+            }
+
+            xArray = x.ToArray();
+            return y.ToArray();
+        }
+
+        public static double MilneMethod(Func<double, double, double> diffFunc, double[] x, double[] y, double step, double accuracy)
+        {
+            double error = 0;
+
+            double yProg = y[y.Length - 4] + 
+                4 * step * (
+                    2 * diffFunc(x[x.Length - 3], y[y.Length - 3])
+                    - diffFunc(x[x.Length - 2], y[y.Length - 2]) 
+                    + 2 * diffFunc(x[x.Length - 1], y[y.Length - 1])
+                    ) / 3;
+
+            double yCorr;
+
+            do
+            {
+                yCorr = y[y.Length - 2] + 
+                    step * (
+                        diffFunc(x[x.Length - 2], y[y.Length -2])
+                        + 4 * diffFunc(x[x.Length - 1], y[y.Length - 1])
+                        + diffFunc(x[x.Length - 1] + step, yProg)
+                    ) / 3;
+                error = Math.Abs(yCorr - yProg) / 29;
+                yProg = yCorr;
+            } while (error > accuracy);
+
+            return yCorr;
+        }
+    }
+}
